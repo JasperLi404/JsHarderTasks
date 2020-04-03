4b757f582c392a760390158f686a44108884b42d
Parakhina Maria <mickeymouse0243@gmail.com>
2020-04-03 17:33:12 +0300

    harder task 07

--- script.js
+++ script.js
@@ -1,62 +1,20 @@
 'use strict';
-let isNumber = (n) => {
-    return !isNaN(parseFloat(n)) && isFinite(n);
-};
-let tries = 10;
-const num = Math.ceil(Math.random() * (100 - 1) + 1);
-console.log(num);
-const GuessNumber = () =>{
-    if(tries > 0) { 
-        let bool = confirm("Угадай число от 1 до 100. Хорошо?");
-        if(bool){
-        
-            let input = +prompt('Угадай');
-            if (isNumber(input)){
-            
-                tries--;
-                if (num < input){
-                    
-                    alert('Загаданное число меньше, осталось попыток:' + tries);
-                    GuessNumber();
-                }
-                else if (num > input){
-                    
-                    alert('Загаданное число больше, осталось попыток:' + tries);
-                    GuessNumber();
-                }
-                else if (num == input ){
-                    let boo = confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?");
-                    if(boo){
-                        GuessNumber();
-                    } else {
-                        alert('bye');
-                    }
-                }
-            } else{
-                alert('Введи число!');
-                GuessNumber();
-            } 
-            
-            
-        
-                
-            
-        
-        
-    }
-       
-        else{
-            alert('Bye');
-        }
-    }else if (tries == 0){
-        let boo = confirm('Попытки закончились, хотите сыграть еще?');
-        if(boo){
-            GuessNumber();
-        } else {
-            alert('bye');
-        }
-        tries=10;
-    }
+let week = ['понедельник','вторник','среда','четверг','пятница','субота','восресенье'];
+let day = (new Date()).getDay();
+console.log(typeof(day));
+
+for(let i = 0;i<week.length;i++){
+    if (i == 6 || i == 5){
+        document.write('<br \/>',week[i].italics());
+
+   }
+   else if (day == i+1){
+        document.write('<br \/>',week[i].bold());
+   }
+   else{
+        document.write('<br \/>',week[i]);
+
+   }
+   
     
 }
-GuessNumber();
