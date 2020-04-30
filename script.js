e423e14b2caff83ea240f5c0ff7527866894a362
Parakhina Maria <mickeymouse0243@gmail.com>
2020-04-30 20:47:06 +0300

    harderTask26 / promise

--- script.js
+++ script.js
@@ -1,49 +1,40 @@
-'use strict';
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', () => {
+    'use strict';
 
-    let up=0,
-         right=0;
-    const DOMElement = function(selector, height, width, bg, fontSize,up,right){
-        let el;
-        if(selector[0] === '#'){
-            el = document.createElement('p');
-            el.id = selector.slice(1);
-        } else if(selector[0] === '.'){
-            el = document.createElement('div');
-            el.classList.add(selector.slice(1));
-            
+    const select = document.getElementById('cars'),
+        output = document.getElementById('output');
+
+    select.addEventListener('change', () => {
+        const car = () => {
+            return new Promise((resolve, reject) => {
+                const request = new XMLHttpRequest();
+                request.open('GET', './cars.json');
+                request.setRequestHeader('Content-type', 'application/json');
+                request.send();
+                request.addEventListener('readystatechange', () => {
+                    if (request.readyState === 4 && request.status === 200) {
+                        return resolve();
+                       
+                    } else {
+                        return reject();
+                    }
+            });
         }
-        height = height;
-        width = width;
-        bg = bg;
-        fontSize = fontSize;
-        el.style.cssText = `height:${height}; 
-                            width:${width};
-                            background-color:${bg};
-                            font-size:${fontSize};
-                            position:absolute;
-                            bottom:${up + 'px'};
-                            right:${right + 'px'};` ;
-        document.body.prepend(el);
-    };
-   
+        });
+        car()
+        .then(() => {
+            const data = JSON.parse(request.responseText);
+                        data.cars.forEach(item => {
+                            if (item.brand === select.value) {
+                                const {brand, model, price} = item;
+                                output.innerHTML = `Тачка ${brand} ${model} <br>
+                                Цена: ${price}$`;
+                            }
+                        });
+        })
+        .catch(() => {
+            output.innerHTML = 'Произошла ошибка';
+        })
+    });
 
-
-    function checkKey(e) {
-        switch(e.keyCode){
-            case 39: right -= 10; break;
-            case 40: up -= 10; break;
-            case 38: up += 10; break;
-            case 37: right +=10; break;
-            default: console.log('dkfhkjdfh');break;
-            
-        }
-        console.log(right, up);
-        document.body.innerHTML='';
-       element = new DOMElement('.eldfgdf','100px','100px','red','2em',up,right); 
-    }
-    
-    document.onkeydown = checkKey;
-    const element = new DOMElement('.eldfgdf','100px','100px','red','2em',up,right); 
-       console.log(element);
-})
+});
